54bd4cd3ff9f9e448ef3bc807d43c004a61b0e46
TAKAHASHI Shuuji <shuuji3@gmail.com>
2021-06-15 22:43:42 +0900

    Change the floating action button text to "+1"

--- lib/main.dart
+++ lib/main.dart
@@ -106,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Icon(Icons.plus_one),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
